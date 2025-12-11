05c17865df70612dce2fdd6814dde3dacb805784
Davidson.dev <davidson.cabista@gmail.com>
2025-12-11 15:32:30 -0300

    Add SkillRepository

---
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.SkillEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillRepository extends JpaRepository<SkillEntity, Long> {
+}
