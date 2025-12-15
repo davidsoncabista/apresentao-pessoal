d2e636daf7c4b392f5cf6d9197c4c0ddfa3dc48b
Davidson.dev <davidson.cabista@gmail.com>
2025-12-15 15:11:49 -0300

    fix: host name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # 1. Inicia o túnel do Cloudflare em segundo plano (&)
 # Ele vai escutar na porta 3306 de DENTRO do container
 echo "Iniciando túnel Cloudflare..."
-cloudflared access tcp --hostname mariadb.davidson.dev.br --url 127.0.0.1:3306 --service-token-id "$CF_ACCESS_CLIENT_ID" --service-token-secret "$CF_ACCESS_CLIENT_SECRET" &
+cloudflared access tcp --hostname db.davidson.dev.br --url 127.0.0.1:3306 --service-token-id "$CF_ACCESS_CLIENT_ID" --service-token-secret "$CF_ACCESS_CLIENT_SECRET" &
 
 # 2. Espera 5 segundos para garantir que o túnel subiu
 sleep 5
